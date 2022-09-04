fe3b554fb36f2575f6c41c31632fe6bd4d9074f7
ravi maithrey <maitrey.ind@gmail.com>
2022-09-04 00:01:02 -0400

    from my pc

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::env;
+use std::fs::File;
+
+fn main() {
+    // read a markdown file and output an HTML file
+    // put getting file as a func
+    // put reading the file and converting as another fun
+    // call both these funs in main
+    let file = File::open(filepath);
+    println!("Hello, world!");
+}
